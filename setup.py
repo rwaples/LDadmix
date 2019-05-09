1996062ee64709ca5e4d8f9e8267eedba5872dfb
Ryan Waples <ryan.waples@gmail.com>
2019-05-09 14:52:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license='GPL 3.0',
     description='Estimate the haplotype frequencies and LD in the ancestral populations of admixed samples.',
     long_description=open('README.md').read(),
-    install_requires=[ 'pandas', 'numpy', 'scipy', 'pandas-plink', 'numba],
+    install_requires=[ 'pandas', 'numpy', 'scipy', 'pandas-plink', 'numba'],
 	#extras_require={'numba_jit_compilation': ['numba']},
     url='https://github.com/rwaples/LDadmix',
     author='Ryan Waples',
